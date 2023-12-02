c6af3abaa5e6af7762c27fbcd7410b8bd2c0cae7
Badr MAMATAHIR <tahir.7badr@gmail.com>
2023-12-02 14:59:06 +0100

    🌱 feat: notify question author when new answers

--- src/controllers/questions.controller.ts
+++ src/controllers/questions.controller.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Response } from 'express';
 import { AnswerQuestionDto, CreateQuestionDto, LikeQuestionDto } from '@dtos/questions.dto';
 import questionService from '@services/questions.service';
+import userService from '@services/users.service';
 import { IQuestion } from '@/models/questions.model';
 import { IAnswer } from '@/models/answers.model';
 import { IFavorite } from '@/models/favorites.model';
 import { RequestWithUser } from '@/interfaces/auth.interface';
+import { IUser } from '@/models/users.model';
+import transporter from '@/libs/mail-transporter';
+import { EMAIL_FROM } from '@/config';
 
 class QuestionController {
   public questionService = new questionService();
+  public usersService = new userService();
 
   public getAllQuestions = async (req: RequestWithUser, res: Response, next: NextFunction) => {
     try {
@@ -40,10 +45,25 @@ class QuestionController {
         userId: req.user._id,
       };
       const answeredQuestion: IAnswer = await this.questionService.answer(requestData);
+
+      const findUser: IUser = await this.usersService.findUserById(answeredQuestion.userId.toString());
+      this.notifyQuestionAuthor(findUser.email, answeredQuestion.content);
+
       res.status(200).json({ data: answeredQuestion, message: 'Question answered successfully' });
     } catch (error) {
       next(error);
     }
+  };
+
+  private notifyQuestionAuthor = (email: string, answer: string) => {
+    transporter.sendMail({
+      from: EMAIL_FROM,
+      to: email,
+      subject: 'Question Answered',
+      html: `
+      <h3>Your question got answered by <pre style="display: inline">"${email}"</pre></h3>
+      "${answer}"`,
+    });
   };
 
   public likeQuestion = async (req: RequestWithUser, res: Response, next: NextFunction) => {
